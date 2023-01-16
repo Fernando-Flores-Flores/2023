37c9408758c6bc09e3cb171fa3d67fe6bb83bd60
fernando.flores@gestora.bo <fernando.flores@gestora.bo>
2023-01-16 18:00:34 -0400

    Cambios Reportes

---
+++ ProyectoMiLunaCielo/BackEnd2023/BackEnd2023/Controllers/ReportesPDFController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using BackEnd2023.dtos;
+using BackEnd2023.dtos.dto_Inventarios;
+using BackEnd2023.Metodos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd2023.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportesPDFController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public ReportesPDFController(ApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+
+
+        [HttpGet("generarPDF")]
+        public async Task<ActionResult<List<dto_Inventario>>> Get(string tipoFormulario)
+        {
+            //var inventario = await context.bd_Inventario.Where(x => x.IdtipoInventario == tipoFormulario).ToListAsync();
+            Reportes reportes = new Reportes();
+            string resultado = reportes.generarReportePDF("argumento1");
+            var response = new ResponseDto<string>()
+            {
+                statusCode = StatusCodes.Status200OK,
+                fechaConsulta = DateTime.Now,
+                codigoRespuesta = 1001,
+                MensajeRespuesta = "CORRECTO",
+                //datos = mapper.Map<List<dto_Inventario>>(inventario)
+                datos = resultado
+            };
+            return Ok(response);
+        }
+    }
+}
---
+++ ProyectoMiLunaCielo/BackEnd2023/BackEnd2023/Metodos/Reportes.cs
@@ -0,0 +1,55 @@
+﻿using BackEnd2023.Entidades.bd.Inventarios;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace BackEnd2023.Metodos
+{
+    public class Reportes
+    {
+        public string generarReportePDF(string nombre)
+        {
+            List<inventario> listaPer = new List<inventario>();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                //Creamos el pdf
+                PdfWriter writer = new PdfWriter(ms);
+                //Luego creamos un PDF
+                using (PdfDocument pdfDoc = new PdfDocument(writer))
+                {
+                    Document doc = new Document(pdfDoc);
+                    crearParafoInicial(doc, "Reporte Inventarios", 20,"center");
+                    Paragraph c1 = new Paragraph("Reporte Inventarios");
+                    //Estilos de Itext7
+                    //c1.SetFontSize(20);
+                    //c1.SetTextAlignment(TextAlignment.CENTER);
+
+                    //doc.Add(c1);
+                    // Se cierra el documento
+                    doc.Close();
+                    //Se cierra la escritura
+                    writer.Close();
+                }
+                return Convert.ToBase64String(ms.ToArray());
+
+            }
+        }
+
+
+        public static void crearParafoInicial(Document doc, string titulo,int fontSize,string alineacion)
+        {
+            Paragraph c1 = new Paragraph(titulo);
+            //Estilos de Itext7
+            c1.SetFontSize(fontSize);
+            switch (alineacion)
+            {
+                case "center":c1.SetTextAlignment(TextAlignment.CENTER); break;
+                case "left":c1.SetTextAlignment(TextAlignment.LEFT); break;
+                case "right": c1.SetTextAlignment(TextAlignment.RIGHT); break;
+                default: c1.SetTextAlignment(TextAlignment.CENTER); break;
+            }
+            doc.Add(c1);
+        }
+    }
+}
