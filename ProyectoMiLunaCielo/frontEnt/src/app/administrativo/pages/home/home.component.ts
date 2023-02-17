ab90225f616ac5e48ac43bd1ef4ce8243b2eb48e
fernando.flores@gestora.bo <fernando.flores@gestora.bo>
2023-02-17 16:22:04 -0400

    Cambio de PC

--- ProyectoMiLunaCielo/frontEnt/src/app/administrativo/pages/home/home.component.ts
+++ ProyectoMiLunaCielo/frontEnt/src/app/administrativo/pages/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../service/login.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  rolUsuario: string = '';
+  constructor(public loginService: LoginService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.rolUsuario = await this.loginService.obtenerCampoJWT('role');
   }
-
 }
--- ProyectoMiLunaCielo/frontEnt/src/app/administrativo/pages/listado-mistrabajos/listado-mistrabajos.component.ts
+++ ProyectoMiLunaCielo/frontEnt/src/app/administrativo/pages/listado-mistrabajos/listado-mistrabajos.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EnvioDatosEntreComponentesService } from '../../service/envio-datos-entre-componentes.service';
 import { LoginService } from '../../service/login.service';
 import { VentasService } from '../../service/ventas.service';
@@ -11,13 +12,16 @@ declare var $: any;
 export class ListadoMistrabajosComponent implements OnInit, AfterViewInit {
   titulo = 'TRABAJOS QUE TENGO ASIGNADO';
   idUsuario = '';
-
+  form: FormGroup;
   listaPrdenesTrabajo: any = [];
   constructor(
     private ventasService: VentasService,
     private envioDatos: EnvioDatosEntreComponentesService,
-    public loginService: LoginService
-  ) {}
+    public loginService: LoginService,
+    private formBuilder: FormBuilder
+  ) {
+    this.builrForm();
+  }
 
   async ngOnInit() {}
 
@@ -46,4 +50,55 @@ export class ListadoMistrabajosComponent implements OnInit, AfterViewInit {
       console.error(error);
     }
   }
+  private builrForm() {
+    this.form = this.formBuilder.group({
+      idOrdenTrabajo: ['', [Validators.required]],
+      idCliente: ['', [Validators.required]],
+      tipoTrabajo: ['', [Validators.required]],
+      descripcion: ['', [Validators.required]],
+      costo: ['', [Validators.required]],
+      observaciones: ['', [Validators.required]],
+      tipoPago: ['', [Validators.required]],
+      idPersonalAsignado: ['', [Validators.required]],
+      fechaEntregaAprox: ['', [Validators.required]],
+      avance: ['', [Validators.required]],
+      estado: ['', [Validators.required]],
+      fechaOrden: ['', [Validators.required]],
+      fechaCreacion: ['', [Validators.required]],
+      fechaModificacion: ['', [Validators.required]],
+    });
+  }
+  /*
+     "idOrdenTrabajo": 22,
+      "idCliente": 66,
+      "tipoTrabajo": "MANUAL",
+      "descripcion": "CINTAS MAGNETICAS",
+      "costo": 500,
+      "observaciones": "300 CINTAS PARA DISCOTECA COMADRES",
+      "tipoPago": "EFECTIVO",
+      "idPersonalAsignado": "f9860231-814e-43d0-ba84-ee18635a1054",
+      "fechaEntregaAprox": "2023-02-16T00:00:00",
+      "avance": 0,
+      "estado": "creado",
+      "fechaOrden": "2023-02-16T00:00:00",
+      "fechaCreacion": "2023-02-16T19:12:34.683528Z",
+      "fechaModificacion": "2023-02-16T19:12:34.683561Z"
+*/
+}
+
+export interface Welcome {
+  idOrdenTrabajo: number;
+  idCliente: number;
+  tipoTrabajo: string;
+  descripcion: string;
+  costo: number;
+  observaciones: string;
+  tipoPago: string;
+  idPersonalAsignado: string;
+  fechaEntregaAprox: Date;
+  avance: number;
+  estado: string;
+  fechaOrden: Date;
+  fechaCreacion: Date;
+  fechaModificacion: Date;
 }
